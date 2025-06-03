1885e2f92e80f85b0c770af90e8b1cff9965e207
Caldwell10 <caldwell.kibe@strathmore.edu>
2025-06-03 22:22:53 +0300

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int days;
+    cout << "Enter the number of days: ";
+    cin >> days;
+
+    int seconds = days *86400;
+    cout << "Days in seconds: " << seconds << endl;
+
+}
